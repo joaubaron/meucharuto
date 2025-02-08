68f104e74831f43448eab0bc1dd869634b136832
JOSE AUGUSTO BARON <jabaron@gmail.com>
2025-02-07 22:01:47 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+function exportarArquivo() {
+    const dados = { mensagem: "Exportação de teste" };
+    const jsonString = JSON.stringify(dados, null, 2);
+    const blob = new Blob([jsonString], { type: "application/json" });
+
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "exportacao.json";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+document.getElementById("btnExportar").addEventListener("click", exportarArquivo);
 	document.addEventListener('DOMContentLoaded', () => {
     displayCigars();
 });
